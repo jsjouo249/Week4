cbe608e87b3971cbe88dca367e89f4f5f94ad8f4
Hyeonsu Lee <lhs951116@gmail.com>
2018-09-28 19:34:40 +0900

    First edit

--- src/main/java/wwww4/App.java
+++ src/main/java/wwww4/App.java
@@ -13,8 +13,17 @@ public class App
         h1.setAge(24);
         h1.setNumber(201413328);
 
+        User h2 = new User();
+        h2.setName("이현수");
+        h2.setAge(24);
+        h2.setNumber(201413339);
+
         System.out.println("이름 : " + h1.getName());
         System.out.println("나이 : " + h1.getAge());
         System.out.println("학번 : " + h1.getNumber());
+
+        System.out.println("이름 : " + h2.getName());
+        System.out.println("나이 : " + h2.getAge());
+        System.out.println("학번 : " + h2.getNumber());
     }
 }
