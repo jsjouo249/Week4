e64793c86ab5655392b7c5ad9fc6feac521764d5
Seunggil <wewe1008@naver.com>
2018-09-28 19:23:48 +0900

    seunghwna first

--- src/main/java/wwww4/App.java
+++ src/main/java/wwww4/App.java
@@ -8,6 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!!" );
+        User h1 = new User();
+        h1.setName("이승환");
+        h1.setAge(24);
+        h1.setNumber(201413328);
+
+        System.out.println("이름 : " + h1.getName());
+        System.out.println("나이 : " + h1.getAge());
+        System.out.println("학번 : " + h1.getNumber());
     }
 }
